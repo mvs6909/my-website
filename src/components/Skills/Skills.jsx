30fb14d94265460b19a73c0909d0391ee7e9406c
Manav Shah <mvs6909@psu.edu>
2022-07-01 14:01:13 -0400

    Added Grid Layout for skill cube

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ImgCube from "./ImgCube";
+import {Container, Row, Col} from "react-bootstrap";
 import "./Skills.css"
 
 function Skills(){
     return (
         <div className="skills">
-            <h3>My skills</h3>
-            <ImgCube />
+        <Container>
+            <Row>
+                <Col>
+                        <h3>My skills</h3>
+                </Col>
+                <Col>
+                    <ImgCube />
+                </Col>
+            </Row>
+        </Container>
         </div>
     )
 }
